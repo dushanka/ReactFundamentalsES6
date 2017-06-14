bbcdecdfef815de61610fe5875d1934f14d75ef8
Clayton Long <clayton.long@agilethought.com>
2017-06-14 15:25:42 -0400

    completed working example

--- src/js/app.babel.js
+++ src/js/app.babel.js
@@ -7,6 +7,7 @@
 
       this.handleBookSelected = this.handleBookSelected.bind(this);
       this.handleContinue = this.handleContinue.bind(this);
+      this.handleAddGame = this.handleAddGame.bind(this);
       this.getInitialState = this.getInitialState.bind(this);
 
       this.state = this.getInitialState();
@@ -31,7 +32,6 @@
                 return <Book onBookSelected={this.handleBookSelected} title={b} key={i} />;
               }, this)}
             </div>
-            {console.log("rerender: " + this.state.bgClass)}
             <div className={"col-md-1 " + this.state.bgClass}>
             </div>
           </div>
@@ -44,13 +44,17 @@
               </div>
             ) : <span />
           }
+          <div className="row">
+            <div className="col-md-12">
+              <input onClick={this.handleAddGame} id="addGameButton"  type="button" value="Add Game" className="btn"/>
+            </div>
+          </div>
         </div>
       );
     }
 
     handleBookSelected(title) {
       let isCorrect = this.state.checkAnswer(title);
-      console.log("Selected book isCorrect? " + isCorrect);
       this.setState({
         bgClass: isCorrect ? 'pass' : 'fail',
         showContinue: isCorrect
@@ -59,6 +63,10 @@
 
     handleContinue() {
       this.setState(this.getInitialState());
+    }
+
+    handleAddGame() {
+      routie('add');
     }
   }
 
@@ -82,6 +90,48 @@
     }
   }
 
+  class AddGameForm extends React.Component {
+    constructor() {
+      super();
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+      return (
+        <div className="row">
+          <div className="col-md-12">
+            <h4>Add Game</h4>
+            <form role="form" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <input ref={(imageUrl) => { this.imageUrl = imageUrl; }} type="text" className="form-control" placeholder="Image Url" />
+              </div>
+              <div className="form-group">
+                <input ref={(answer1) => {this.answer1 = answer1; }} type="text" className="form-control" placeholder="Answer 1" />
+              </div>
+              <div className="form-group">
+                <input ref={(answer2) => {this.answer2 = answer2; }} type="text" className="form-control" placeholder="Answer 2" />
+              </div>
+              <div className="form-group">
+                <input ref={(answer3) => {this.answer3 = answer3; }} type="text" className="form-control" placeholder="Answer 3" />
+              </div>
+              <div className="form-group">
+                <input ref={(answer4) => {this.answer4 = answer4; }} type="text" className="form-control" placeholder="Answer 4" />
+              </div>
+              <button type="submit" className="btn btn-default">Submit</button>
+            </form>
+          </div>
+        </div>
+      );
+    }
+
+    handleSubmit() {
+      this.props.onGameFormSubmitted(getRefs(this));
+      return false;
+    }
+  }
+
+  //<editor-fold desc="data properties">
   let data = [
     {
       name: 'Mark Twain',
@@ -102,29 +152,90 @@
       name: 'Charles Dickens',
       imageUrl: 'images/authors/charlesdickens.jpg',
       books: ['Great Expectations', 'A Tale of Two Cities']
+    },
+    {
+      name: 'William Shakespeare',
+      imageUrl: 'images/authors/williamshakespeare.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    },
+    {
+      name: 'J.K. Rowling',
+      imageUrl: 'images/authors/jkrowling.jpg',
+      imageSource: 'Wikimedia Commons',
+      imageAttribution: 'Daniel Ogren',
+      books: ['Harry Potter and the Sorcerers Stone']
     }
   ];
 
-  data.selectGame = function() {
-    let books = _.shuffle(this.reduce(function(p, c, i) {
+  var selectGame = function() {
+    let books = _.shuffle(this.reduce((p, c, i) => {
       return p.concat(c.books);
     }, [])).slice(0, 4);
     let answer = books[_.random(books.length - 1)];
     return {
       books: books,
       author: _.find(this, function(author) {
-        return author.books.some(function(title) {
+        return author.books.some((title) => {
           return title === answer;
         });
       }),
       checkAnswer: function(title) {
-        return this.author.books.some(function(t) {
+        return this.author.books.some((t) => {
           return t == title;
         });
       }
     };
   };
 
-  ReactDOM.render(<Quiz data={data}/>, document.getElementById('app'));
+  data.selectGame = selectGame;
+  //</editor-fold>
+
+  //<editor-fold desc="routing">
+  routie({
+    '': () => {
+      ReactDOM.render(<Quiz data={data}/>, document.getElementById('app'));
+    },
+    'add': () => {
+      ReactDOM.render(<AddGameForm onGameFormSubmitted={handleAddGameFormSubmitted}/>, document.getElementById('app'));
+    }
+  });
+  //</editor-fold>
+
+  //<editor-fold desc="top-level scoped functions">
+  function handleAddGameFormSubmitted(data) {
+    let quizData = [{
+      imageUrl: data.imageUrl,
+      books: [data.answer1, data.answer2, data.answer3, data.answer4]
+    }];
+    quizData.selectGame = selectGame;
+    ReactDOM.render(<Quiz data={quizData} />, document.getElementById('app'));
+  };
+
+  var getRefs = (component) => {
+    let result = {
+      imageUrl: component.imageUrl.value,
+      answer1: component.answer1.value,
+      answer2: component.answer2.value,
+      answer3: component.answer3.value,
+      answer4: component.answer4.value
+    };
+    return result;
+  };
+  //</editor-fold>
+
+  //<editor-fold desc="prop-types validation">
+  Quiz.propTypes = {
+    data: PropTypes.array.isRequired
+  };
+
+  Book.propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
+  AddGameForm.propTypes = {
+    onGameFormSubmitted: PropTypes.func.isRequired
+  };
+  //</editor-fold>
 
 })();
